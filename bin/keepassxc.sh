d0a042347cb496ecce53a1ace225ac483dde5ae2
Akira Heid <akira710@protonmail.com>
2021-05-23 22:30:37 -0400

    fix "device/resource busy" error
    When editing a db file loaded via argument, changes to the db would fail
    because of a "device/resource busy" error. Mounting the directory the
    file is in and using that instead of mounting the file directly seems to
    resolve the issue.

--- bin/keepassxc.sh
+++ bin/keepassxc.sh
@@ -7,12 +7,13 @@ mkdir -p ${CONFIG_DIR}
 
 ARG=""
 VOL=""
-if [ $# != 0 ]; then
+if [ $# == 1 ]; then
 	ARG="/root/data/$(basename $1)"
-	VOL="-v $(readlink -f $1):$ARG"
+	VOL="-v $(dirname $(readlink -f $1)):/root/data"
 fi
 
-podman run -d \
+podman run \
+	-d \
 	-e DISPLAY=unix${DISPLAY} \
 	--name keepassxc-$(uuidgen) \
 	--rm \
@@ -22,26 +23,3 @@ podman run -d \
 	-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
 	$VOL \
 	localhost/keepassxc $ARG
-
-#	# Stop the existing container if a file is not specified
-#	if [ $# == 0 ]; then
-#		stopdel keepassxc
-#	fi
-#
-#	ARG="/root/.config/keepassxc/keepassxc.kdbx"
-#	NAME="--name keepassxc"
-#	VOL=""
-#	if [ $# == 1 ]; then
-#		ARG="/root/data/$(basename $1)"
-#		NAME=""
-#		VOL="-v $(readlink -f $1):$ARG"
-#	fi
-#
-#	podman run -d --rm $NAME \
-#		-e DISPLAY=unix${DISPLAY} \
-#		-v ${HOME}/syncthing/computers/keepassxc/:/root/.config/keepassxc/ \
-#		-v /etc/machine-id:/etc/machine-id:ro \
-#		-v /tmp/.X11-unix:/tmp/.X11-unix \
-#		-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
-#		$VOL \
-#		localhost/keepassxc $ARG
